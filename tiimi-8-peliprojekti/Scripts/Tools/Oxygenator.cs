c815a31fc76d100dab22c3473b56632184e96105
Antti Pihlajamäki <antti.pihlajamaki@tuni.fi>
2026-04-02 13:56:04 +0300

    Bubble effect and sho categories

--- tiimi-8-peliprojekti/Scripts/Tools/Oxygenator.cs
+++ tiimi-8-peliprojekti/Scripts/Tools/Oxygenator.cs
@@ -6,7 +6,9 @@ public partial class Oxygenator : Tool
 	private bool _isActive = false; // Whether the tool is being used
 	[Export] private float _maxPower = 10.0f; // Minimum/maximum oxygen that can be removed/added per second
 	private float _basePower = 0f; // The default power of the tool, also used as the amount of change when changing power
-	private float _power; // Current power of the tool
+	private float _power = 0f; // Current power of the tool
+
+	[Export] GpuParticles2D _particles;
 
 	[Export] private VSlider powerSlider;
     public override void _Ready()
@@ -17,6 +19,8 @@ public partial class Oxygenator : Tool
 		powerSlider.MaxValue = _maxPower;
 		powerSlider.Value = _basePower;
 
+		powerSlider.Value = _power;
+		SetPower(true);
 		powerSlider.DragEnded += SetPower;
 	}
 
@@ -33,31 +37,36 @@ public partial class Oxygenator : Tool
         if (@event is InputEventScreenTouch)
 		{
 			InputEventScreenTouch screenTouch = @event as InputEventScreenTouch;
-			if (screenTouch.IsPressed())
+			ToolFunction(screenTouch);
+		}
+
+		if (@event is InputEventScreenDrag)
+		{
+			InputEventScreenDrag screenDrag = @event as InputEventScreenDrag;
+			_particles.GlobalPosition = (GetViewport().GetCamera2D().GlobalPosition - GetViewport().GetVisibleRect().Size/2 + screenDrag.Position)/GetViewport().GetCamera2D().Zoom;
+		}
+    }
+
+	protected override void ToolFunction(InputEventScreenTouch screenTouch) // Changes tool from inactive to active and back again
+	{
+		if (screenTouch.IsPressed())
 			{
 				_isActive = true;
+				_particles.Emitting = true;
+				_particles.GlobalPosition = (GetViewport().GetCamera2D().GlobalPosition - GetViewport().GetVisibleRect().Size/2 + screenTouch.Position)/GetViewport().GetCamera2D().Zoom;
 			}
 			else if(screenTouch.IsReleased())
 			{
 				_isActive = false;
+				_particles.Emitting = false;
 			}
-		}
-    }
-
-	protected override void ToolFunction(InputEventScreenTouch @event) // Changes tool from inactive to active and back again
-	{
 	}
 	private void SetPower(bool valueChanged)
 	{
 		if (valueChanged)
 		{
 			_power = Mathf.Clamp((float)powerSlider.Value, -_maxPower, _maxPower);
+			_particles.AmountRatio = Mathf.Abs(_power) / _maxPower;
 		}
-	}
-
-	
-	protected override string Info() // ! Temporary method to get info about the tool while UI gets added !
-	{
-		return GetType() + " = ( Power: " + _power + " )";
 	}
 }

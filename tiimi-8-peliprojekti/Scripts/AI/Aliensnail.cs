489d3ae66486c8520e95b88493d4e13e7488f622
ukonatte <ukonahoatte@gmail.com>
2026-04-13 15:30:29 +0300

    added alien snail

---
+++ tiimi-8-peliprojekti/Scripts/AI/Aliensnail.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public partial class Aliensnail : Alien
+{
+	public override void _PhysicsProcess(double delta)
+	{
+		if(GameManager.Instance.ActiveAquarium._objects.Count > 0)
+		{
+			foreach
+		}
+
+		base._PhysicsProcess(delta);
+	}
+}
